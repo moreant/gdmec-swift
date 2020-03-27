bcaa2c959671d220a48ef4148e6e4c375db02b40
admin <admin@moreantdeMac.local>
2020-03-27 00:13:34 -0700

    Initial Commit

---
+++ moreant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moreant
+//
+//  Created by admin on 2020/3/27.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
