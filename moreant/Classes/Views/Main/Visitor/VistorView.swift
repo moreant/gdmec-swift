8e7f405702ac316ef287a9be076023178cdd1e9b
moreant <moreant@foxmail.com>
2020-05-18 22:40:10 +0800

    complete: 4-4.3

--- moreant/Classes/Views/Main/Visitor/VistorView.swift
+++ moreant/Classes/Views/Main/Visitor/VistorView.swift
@@ -14,6 +14,16 @@ class VistorView: UIView {
     
     public lazy var homeIconView:UIImageView = UIImageView(image:UIImage(named:"visitordiscover_feed_image_house"))
     
+    public lazy var messageLable:UILabel = {
+        let lable = UILabel()
+        lable.text="关注一些人，回这里看看有什么惊喜"
+        lable.textColor=UIColor.darkGray
+        lable.font=UIFont.systemFont(ofSize: 14)
+        lable.numberOfLines=0
+        lable.textAlignment=NSTextAlignment.center
+        return lable
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -40,6 +50,7 @@ extension VistorView
     {
         addSubview(iconView)
         addSubview(homeIconView)
+        addSubview(messageLable)
         for v in subviews
         {
             v.translatesAutoresizingMaskIntoConstraints=false
@@ -49,6 +60,11 @@ extension VistorView
         addConstraint(NSLayoutConstraint(item: iconView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1.0, constant: -60))
         addConstraint(NSLayoutConstraint(item: homeIconView, attribute: .centerX, relatedBy: .equal, toItem: iconView, attribute: .centerX, multiplier: 1.0, constant: 0))
         addConstraint(NSLayoutConstraint(item: homeIconView, attribute: .centerY, relatedBy: .equal, toItem: iconView, attribute: .centerY, multiplier: 1.0, constant: 0))
+        
+        addConstraint(NSLayoutConstraint(item: messageLable, attribute: .centerX, relatedBy: .equal, toItem: iconView, attribute: .centerX, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: messageLable, attribute: .centerY, relatedBy: .equal, toItem: iconView, attribute: .bottom, multiplier: 1.0, constant: 16))
+        addConstraint(NSLayoutConstraint(item: messageLable, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 224))
+        addConstraint(NSLayoutConstraint(item: messageLable, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 36))
         
     }
 }
