d8dabc3d08aabfd0e3eaf678b29d3748c88fa8bb
moreant <moreant@foxmail.com>
2020-06-22 01:43:25 +0800

    complete: 7-1.1

--- moreant/AppDelegate.swift
+++ moreant/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         window = UIWindow(frame:UIScreen.main.bounds)
         window?.backgroundColor=UIColor.white
-        window?.rootViewController=MainViewController()
+        window?.rootViewController=NewFeatureViewController()
         window?.makeKeyAndVisible()
         return true
     }
---
+++ moreant/Classes/Views/Main/NewFeature/NewFeatureViewController.swift
@@ -0,0 +1,148 @@
+//
+//  NewFeatureViewController.swift
+//  moreant
+//
+//  Created by admin on 2020/6/22.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class NewFeatureViewController: UICollectionViewController {
+    
+    private let WBNewFeatureViewCellId = "WBNewFeatureViewCellId"
+    
+    public let WBNewFeatureImageCount = 4
+    
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize=UIScreen.main.bounds.size
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .horizontal
+        
+        super.init(collectionViewLayout: layout)
+        
+        collectionView?.isPagingEnabled = true
+        collectionView?.bounces = false
+        
+        collectionView?.showsHorizontalScrollIndicator = false
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(NewFeatureCell.self, forCellWithReuseIdentifier: WBNewFeatureViewCellId)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return WBNewFeatureImageCount
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WBNewFeatureViewCellId, for: indexPath) as! NewFeatureCell
+        cell.imageIndex = indexPath.item
+    
+        // Configure the cell
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
+
+class NewFeatureCell:UICollectionViewCell
+{
+    public lazy var iconView:UIImageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame:frame)
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func setupUI()
+    {
+        addSubview(iconView)
+        iconView.frame = bounds
+    }
+    
+    public var imageIndex:Int = 0
+    {
+        didSet{
+            iconView.image = UIImage(named:"new_feature_\(imageIndex+1)")
+        }
+    }
+    
+}
+
